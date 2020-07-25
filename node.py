81dc965d6baf2057395ba7f913bb6a203ddc43a0
Makai Post <postmakai@gmail.com>
2020-07-24 18:23:15 -0700

    added path finder

--- node.py
+++ node.py
@@ -1,4 +1,6 @@
-from world.world import *
+from __future__ import annotations
+
+from world import *
 
 
 def get_xy(point):
@@ -12,14 +14,25 @@ def get_xy(point):
         return point, point
 
 
-def get_distance_from(point1, point2):
+def get_distance(point1, point2):
     x1, y1 = get_xy(point1)
     x2, y2 = get_xy(point2)
 
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
-def get_radians_from(point1, point2, from0to2=False):
+# def get_radians(point1, point2, from0to2=False):
+#     x1, y1 = get_xy(point1)
+#     x2, y2 = get_xy(point2)
+#
+#     radians = math.atan2(x2 - x1, y2 - y1) / math.pi
+#
+#     if from0to2:
+#         return 2 + radians if radians < 0 else radians
+#     else:
+#         return radians
+
+def get_radians(point1, point2, from0to2=False):
     x1, y1 = get_xy(point1)
     x2, y2 = get_xy(point2)
 
@@ -31,8 +44,8 @@ def get_radians_from(point1, point2, from0to2=False):
         return radians
 
 
-def get_angle_from(point1, point2, from0to360=False):
-    return round(get_radians_from(point1, point2, from0to360) * 180, 8)
+def get_angle(point1, point2, from0to360=False):
+    return round(get_radians(point1, point2, from0to360) * 180, 8)
 
 
 def get_point(point1, angle, distance) -> Node:
@@ -51,13 +64,13 @@ class Node:
         return self.x, self.y
 
     def get_distance_from(self, other):
-        return get_distance_from(self, other)
+        return get_distance(self, other)
 
     def get_radians_from(self, other, from0to2=False):
-        return get_radians_from(self, other, from0to2)
+        return get_radians(self, other, from0to2)
 
     def get_angle_from(self, other, from0to360=False):
-        return get_angle_from(self, other, from0to360)
+        return get_angle(self, other, from0to360)
 
     def get_point(self, angle, distance) -> Node:
         return get_point(self, angle, distance)
@@ -102,32 +115,32 @@ class Node:
     def __eq__(self, other):
         x1, y1 = get_xy(self)
         x2, y2 = get_xy(other)
-        return x1 == x2, y1 == y2
+        return x1 == x2 and y1 == y2
 
     def __ne__(self, other):
         x1, y1 = get_xy(self)
         x2, y2 = get_xy(other)
-        return x1 != x2, y1 != x2
+        return x1 != x2 and y1 != x2
 
     def __gt__(self, other):
         x1, y1 = get_xy(self)
         x2, y2 = get_xy(other)
-        return x1 > x2, y1 > y2
+        return x1 > x2 and y1 > y2
 
     def __ge__(self, other):
         x1, y1 = get_xy(self)
         x2, y2 = get_xy(other)
-        return x1 >= x2, y1 >= y2
+        return x1 >= x2 and y1 >= y2
 
     def __lt__(self, other):
         x1, y1 = get_xy(self)
         x2, y2 = get_xy(other)
-        return x1 < x2, y1 < y2
+        return x1 < x2 and y1 < y2
 
     def __le__(self, other):
         x1, y1 = get_xy(self)
         x2, y2 = get_xy(other)
-        return x1 <= x2, y1 <= y2
+        return x1 <= x2 and y1 <= y2
 
     def __contains__(self, item):
         return item in (self.x, self.y)
@@ -153,6 +166,9 @@ class Node:
     def __iter__(self):
         return zip([self.x, self.y])
 
+    def __hash__(self):
+        return self.x, self.y
+
     def __str__(self):
         return f'({self.x}, {self.y})'
 
@@ -162,16 +178,104 @@ class Node:
 
 class PathNode(Node):
     def __init__(self, id: int, u: int, v: int):
+        super().__init__(u * METER, v * METER)
         self.id = id
         self.u = u
         self.v = v
-        self.x = u * METER
-        self.y = v * METER
-        self.paths = []
+        self.paths = {}
+
+    def find_shortest_route(self, target: PathNode) -> Route:
+        all_nodes = [self]
+        new_nodes = [self]
+        routes = {'start': {}, 'end': {}}
+
+        while len(new_nodes):
+            old_nodes = new_nodes.copy()
+            new_nodes.clear()
+            for node in old_nodes:
+                for path in node.paths.values():
+                    route = path.find_route(target)
+
+                    if route.start not in routes['start']:
+                        routes['start'][route.start] = {route.end: route}
+                    else:
+                        routes['start'][route.start][route.end] = route
+
+                    if route.end not in routes['end']:
+                        routes['end'][route.end] = {route.start: route}
+                    else:
+                        routes['end'][route.end][route.start] = route
+
+                    if route.end not in all_nodes:
+                        all_nodes.append(route.end)
+                        new_nodes.append(route.end)
+
+        def update_chart(node: PathNode, distance=None, previous: PathNode = None, have_visited=None):
+            if node in chart:
+                if distance is not None:
+                    chart[node]['distance'] = distance
+                if previous is not None:
+                    chart[node]['previous'] = previous
+                if node in unvisited and have_visited is not None and have_visited:
+                    del unvisited[node]
+            else:
+                info = {'distance': distance, 'previous': previous}
+                chart[node] = info
+                unvisited[node] = info
+
+        chart = {}
+        unvisited = {}
+
+        def next():
+            return sorted(unvisited.items(), key=lambda x: x[1]['distance'])[0][0]
+
+        update_chart(self, 0)
+        [update_chart(node, MAX32) for node in all_nodes[1:]]
+
+        while len(unvisited):
+            node = next()
+            for route in routes['start'][node].values():
+                old_dist = chart[route.end]['distance']
+                new_dist = chart[route.start]['distance'] + route.length
+                if new_dist < old_dist:
+                    update_chart(route.end, new_dist, route.start)
+            update_chart(node, have_visited=True)
+
+        def join_routes(route1: Route, route2: Route):
+            if route1 is None and route2 is None:
+                return None
+            if route1 is None:
+                return route2
+            if route2 is None:
+                return route1
+
+            if route1.end == route2.start:
+                route1.end = route2.end
+                route1.nodes = route1.nodes + route2.nodes[1:]
+                route1.length += route2.length
+            elif route1.start == route2.end:
+                route1.start = route2.start
+                route1.nodes = route2.nodes + route1.nodes[1:]
+                route1.length += route2.length
+            return route1
+
+        previous: PathNode = target
+        info = chart[previous]
+        shortest: Route = None
+        while info['previous'] is not None:
+            current: Route = routes['start'][info['previous']][previous]
+            shortest = join_routes(shortest, current)
+            previous = info['previous']
+            info = chart[previous]
+
+        return shortest
 
     def __repr__(self):
         ids = f'[{self.id}]' if self.id != -1 else ""
-        return f'Node{ids}({self.x}, {self.y})'
+        return f'PathNode{ids}({self.x}, {self.y})'
+
+    def __hash__(self):
+        return self.id if self.id != -1 else super().__hash__()
 
 
 class Path:
@@ -187,7 +291,11 @@ class Path:
         return x1, y1, x2, y2
 
     def get_angle(self, from0to360=False):
-        return get_angle_from(self.node1, self.node2, from0to360)
+        return get_angle(self.node1, self.node2, from0to360)
+
+    @property
+    def length(self):
+        return self.node1.get_distance_from(self.node2)
 
     def __str__(self):
         return f'({str(self.node1)}, {str(self.node2)})'
@@ -195,3 +303,43 @@ class Path:
     def __repr__(self):
         ids = f'[{self.id}]' if self.id != -1 else ""
         return f'Path{ids}({self.node1.x}, {self.node1.y}, {self.node2.x}, {self.node2.y})'
+
+    def find_route(self, target: PathNode, route: Route = None):
+        if route is None:
+            route = Route(self)
+        else:
+            route.add_path(self)
+
+        if self.node2 == target:
+            return route
+
+        count = len(route.end.paths)
+
+        if count == 2:
+            for id, path in route.end.paths.items():
+                if id != self.id:
+                    route = path.find_route(target, route)
+        return route
+
+
+class Route:
+    def __init__(self, path: Path):
+        if path is not None:
+            self.start: PathNode = path.node1
+            self.end: PathNode = path.node2
+            self.length = path.length
+            self.nodes = [path.node1, path.node2]
+
+    def add_path(self, path: Path):
+        self.end = path.node2
+        self.length += path.length
+        self.nodes.append(path.node2)
+
+    def __len__(self):
+        return len(self.nodes)
+
+    def __repr__(self):
+        return f'Route(start:{self.start}, end:{self.end}, length:{self.length}, nodes:{len(self.nodes)})'
+
+    def __str__(self):
+        return f'({self.start}, {self.end})'
--- world.py
+++ world.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 from tkinter import Tk, Canvas, Event
 from numpy import random as np_rand
 
-from my_global import *
+from my_globals import *
+from node import *
 
 METER = 10
 LANE_WIDTH = METER * 2
 PATH_COLOR = '#656565'
 ACTIVE_COLOR = '#858585'
+
 
 class World:
     def __init__(self, title: str = 'Path Finder', width: int = 1000, height: int = 800, seed: int = -1):
@@ -34,6 +36,9 @@ class World:
         self.circle = None
         self.line = None
         self.last_node = None
+
+        self.route_start = None
+        self.route_end = None
 
         self.built = False
 
@@ -68,10 +73,10 @@ class World:
             id = self.rand.randint(0, MAX32)
             if id not in self.paths:
                 path1 = Path(id, node1, node2)
-                node1.paths.append(path1)
+                node1.paths[id] = path1
 
                 path2 = Path(id, node2, node1)
-                node2.paths.append(path2)
+                node2.paths[id] = path2
 
                 self.paths[id] = (path1, path2)
                 return path1, path2
@@ -83,10 +88,10 @@ class World:
 
     def get_nodes_near(self, x, y, distance):
         for node in self.path_nodes.values():
-            if node.get_distance_from((x, y)) <= distance:
+            if node.get_distance((x, y)) <= distance:
                 yield node
 
-    def get_nearest_path_node(self, point):
+    def get_nearest_path_node(self, point) -> Node:
         nearest: PathNode = None
         max_distance = MAX32
         point = get_xy(point)
@@ -110,6 +115,7 @@ class World:
                     x2, y2 = path[1].node1
                     self.canvas.create_line(x1, y1, x2, y2, width=LANE_WIDTH * 2, fill=PATH_COLOR)
 
+            def make_lines():
                 # draw lines
                 for path in self.paths.values():
                     n1: PathNode = path[0].node1
@@ -119,14 +125,16 @@ class World:
                     a2 = n2.get_angle_from(n1)
                     o1 = n1.get_point(a1, LANE_WIDTH * (1.75 if len(n1.paths) > 2 else 0))
                     o2 = n2.get_point(a2, LANE_WIDTH * (1.75 if len(n2.paths) > 2 else 0))
-                    o1_1 = o1.get_point(a1 - 90, LANE_WIDTH)
-                    o1_2 = o1.get_point(a1 + 90, LANE_WIDTH)
-                    o2_1 = o2.get_point(a2 - 90, LANE_WIDTH)
-                    o2_2 = o2.get_point(a2 + 90, LANE_WIDTH)
+                    if len(n1.paths) > 2:
+                        o1_1 = o1.get_point(a1 - 90, LANE_WIDTH)
+                        o1_2 = o1.get_point(a1 + 90, LANE_WIDTH)
+                        self.canvas.create_line(*o1_1, *o1_2, width=METER * 0.2, fill='white', dash=(1, 1))
+                    if len(n2.paths) > 2:
+                        o2_1 = o2.get_point(a2 - 90, LANE_WIDTH)
+                        o2_2 = o2.get_point(a2 + 90, LANE_WIDTH)
+                        self.canvas.create_line(*o2_1, *o2_2, width=METER * 0.2, fill='white', dash=(1, 1))
 
                     self.canvas.create_line(*o1, *o2, width=METER * 0.1, fill='white', dash=(10, 5))
-                    self.canvas.create_line(*o1_1, *o1_2, width=METER * 0.2, fill='white', dash=(1, 1))
-                    self.canvas.create_line(*o2_1, *o2_2, width=METER * 0.2, fill='white', dash=(1, 1))
 
             def make_nodes():
                 # draw nodes
@@ -144,7 +152,8 @@ class World:
                     self.creating_new_path = True
                     coords = *(self.last_node - LANE_WIDTH), *(self.last_node + LANE_WIDTH)
                     self.canvas.create_oval(*coords, fill=ACTIVE_COLOR, width=0)
-                    self.line = self.canvas.create_line(*self.last_node, *mouse, width=LANE_WIDTH * 2, fill=ACTIVE_COLOR)
+                    self.line = self.canvas.create_line(*self.last_node, *mouse, width=LANE_WIDTH * 2,
+                                                        fill=ACTIVE_COLOR)
                     coords = *(mouse - LANE_WIDTH), *(mouse + LANE_WIDTH)
                     self.circle = self.canvas.create_oval(*coords, fill=ACTIVE_COLOR, width=0, tag='new_node')
 
@@ -169,11 +178,11 @@ class World:
 
                 def on_hover(event: Event):
                     mouse = Node(event.x, event.y)
-                    nearest = self.get_nearest_path_node(mouse)
+                    # nearest = self.get_nearest_path_node(mouse)
 
                 def on_leave(event):
                     mouse = Node(event.x, event.y)
-                    nearest = self.get_nearest_path_node(mouse)
+                    # nearest = self.get_nearest_path_node(mouse)
 
                 self.canvas.tag_bind('path_node', '<Enter>', on_hover)
                 self.canvas.tag_bind('path_node', '<Leave>', on_leave)
@@ -183,19 +192,62 @@ class World:
 
             make_paths()
             make_nodes()
+            make_lines()
+            self.draw_route()
 
     def draw(self):
         self.build()
 
+    def draw_route(self, start: PathNode = None, end: PathNode = None):
+        if start is not None:
+            self.route_start = start
+        if end is not None:
+            self.route_end = end
+
+        if self.route_start is not None and self.route_end is not None:
+            route: Route = self.route_start.find_shortest_route(self.route_end)
+            path_width = LANE_WIDTH / 3
+            radius = path_width / 2
+            path_color = '#BADA55'
+
+            cnt = len(route.nodes)
+            n1 = a1 = o1 = 0
+            for n in range(cnt):
+                if n == 0:
+                    n1 = route.nodes[n]
+                    n2 = route.nodes[n + 1]
+                    a1 = get_angle(n1, n2)
+                    o1 = get_point(get_point(n1, a1, METER), a1 - 90, LANE_WIDTH / 2)
+                    self.canvas.create_oval(*o1-radius, *o1+radius, fill=path_color, width=0)
+                elif n < cnt - 1:
+                    n1 = route.nodes[n]
+                    n2 = route.nodes[n + 1]
+                    a0 = a1
+                    a2 = get_angle(n1, n2)
+                    a1 = (a0 + a2) / 2
+                    o0 = o1
+                    o1 = get_point(n1, a1 - 90, LANE_WIDTH / 2)
+                    self.canvas.create_oval(*o1-radius, *o1+radius, fill=path_color, width=0)
+                    self.canvas.create_line(*o0, *o1, fill=path_color, width=path_width)
+                else:
+                    n1 = route.nodes[n]
+                    a0 = a1
+                    o0 = o1
+                    o1 = get_point(n1, a0 - 90, LANE_WIDTH / 2)
+                    self.canvas.create_oval(*o1-radius, *o1+radius, fill=path_color, width=0)
+                    self.canvas.create_line(*o0, *o1, fill=path_color, width=path_width)
+
 
 if __name__ == '__main__':
-    world = World(width=3000, height=1000, seed=0)
-    n1 = world.add_path_node(5, 5)
-    n2 = world.add_path_node(5, 25)
-    n3 = world.add_path_node(45, 25)
-    n4 = world.add_path_node(20, 50)
-    world.add_path(n1, n2)
-    world.add_path(n2, n3)
-    world.add_path(n2, n4)
+    world = World(width=1720, height=1400)
+    g = 5
+    points = [[5*g, 5*g], [20*g, 5*g], [5*g, 15*g], [20*g, 15*g], [15*g, 25*g], [30*g, 20*g], [30*g, 25*g]]
+    node_list = [world.add_path_node(*point) for point in points]
+    pairs = [[0, 1], [0, 2], [1, 3], [2, 3], [2, 4], [3, 4], [3, 5], [4, 6], [5, 6]]
+    paths = [world.add_path(*[node_list[pair[0]], node_list[pair[1]]]) for pair in pairs]
+    # [print(path[0].node1.id, path[0].node2.id) for path in paths]
+    # [print(node) for node in node_list]
+    # route = node_list[0].find_shortest_route(node_list[-1])
     world.draw()
+    world.draw_route(node_list[0], node_list[-1])
     world.root.mainloop()
